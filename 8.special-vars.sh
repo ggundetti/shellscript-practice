9e32aecd696ec5d6150730b95fdf425fa05190d5
Gopal <gopal.gundeti@gmail.com>
2025-10-12 19:29:09 +0530

    special Vars

--- 8.special-vars.sh
+++ 8.special-vars.sh
@@ -3,3 +3,6 @@
 
 echo "My favourite leaders are: $@"
 echo "My executed script name: $0"
+echo "who is the owner of script: $whoami"
+echo "current working directory: $PWD"
+echo "home directory of user: $HOME"
