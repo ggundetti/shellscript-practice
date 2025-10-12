0dc179fef12401c68dece63aa7cbf1311cdd4a7d
Gopal <gopal.gundeti@gmail.com>
2025-10-12 19:34:35 +0530

    special Vars

--- 8.special-vars.sh
+++ 8.special-vars.sh
@@ -6,3 +6,7 @@ echo "My executed script name: $0"
 echo "who is the owner of script: $USER"
 echo "current working directory: $PWD"
 echo "home directory of user: $HOME"
+echo "the process ID of the script: $$"
+sleep 10 &
+echo "the backgroudn of PID: $!"
+
