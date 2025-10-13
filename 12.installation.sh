31ebeba673abb1014cec3e081ef7c3a87cece8b7
Gopal <gopal.gundeti@gmail.com>
2025-10-13 11:25:48 +0530

    installation

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run the script with root previlages"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "The installation is failed"
+    exit 1
 else
     echo "The Installation of MySQL is SUCCESS"
 fi
