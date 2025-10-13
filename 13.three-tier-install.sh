68c9098c70f4d931e9d925862113586060bf8375
Gopal <gopal.gundeti@gmail.com>
2025-10-13 11:41:58 +0530

    installation

---
+++ 13.three-tier-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run with root previlages"
+    exit 1
+
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "ERROR:: MySQL Installation failed"
+    exit 1
+else
+    echo "The installation of MySQL is SUCCESS"
+
+dnf install nginix -y
+
+if [ $? -ne 0 ]; then
+    echo ""ERROR:: NGINIX installation failed"
+    exit 1
+else
+    echo "The NGINIX installation is SUCCESS"
+
+dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: mongoDB installation is failed"
+    exit 1
+else
+    echo "Mongo DB installation is success"
+
+fi
