7763e2fd6bfb215ef1063df0148aedb90426c026
Gopal <gopal.gundeti@gmail.com>
2025-10-13 10:29:32 +0530

    conditions

---
+++ 11.conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter the Number:"
+read NUMBER
+if [ $($NUMBER % 2) -eq 0 ]; then
+    echo "the given $NUMBER is EVEN"
+else
+    echo "the given $NUMBER is ODD"
+fi
+
