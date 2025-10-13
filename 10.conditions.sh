bb1297018dc20eb87a013f059401845d94eec677
Gopal <gopal.gundeti@gmail.com>
2025-10-13 10:12:02 +0530

    conditions

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -4,6 +4,6 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "given $NUMBER is less than 10"
-elseif [ $NUMBER -gt 10 ]; then
+else [ $NUMBER -gt 10 ]; then
     echo "given $NUMBER is greater than 10"
 fi
